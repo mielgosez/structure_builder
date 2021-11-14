34ec0f9d3a0f2c3e3463340728863d3eabae80c8
Jose Mielgo <mielgosez@gmail.com>
2021-11-14 14:08:01 +0100

    Fixing unit tests

--- tests/unit/test_primitives.py
+++ tests/unit/test_primitives.py
@@ -5,4 +5,4 @@ from structure_builder.primitives import *
 def test_number_of_points_in_circle():
     n_points = random.randint(5, 10)
     new_circle = CircleMesh(radius=1, n_points=n_points, height=1)
-    assert len(new_circle.cloud_points['x'].keys()) == n_points
+    assert len(new_circle.cloud_points.coordinates_dict['x'].keys()) == n_points
