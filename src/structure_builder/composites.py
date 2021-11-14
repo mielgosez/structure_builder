8e1755218cdaed3f836ab33ce91bcc1eece737fa
Jose Mielgo <mielgosez@gmail.com>
2021-11-14 14:58:24 +0100

    Implementing cubic mesh and creating integration test to validate its shape quickly

--- src/structure_builder/composites.py
+++ src/structure_builder/composites.py
@@ -1,4 +1,4 @@
-from structure_builder.primitives import BaseMesh, CircleMesh
+from structure_builder.primitives import BaseMesh, CircleMesh, RectangularMesh
 
 
 class ColumnMesh(BaseMesh):
@@ -16,9 +16,14 @@ class ColumnMesh(BaseMesh):
         self.create_cloud_points()
 
     def create_cloud_points(self):
+        """
+        Column is created from bottom to top in the loop.
+        :return: cloud points is updated.
+        """
         diameter = 2*self.radius
         smallest_diameter = 7/8*diameter
         greatest_diameter_difference = 1/8*diameter
+        previous_cloud = None
         for i in range(8):
             diameter_difference = (7-i)*greatest_diameter_difference/7
             local_diameter = smallest_diameter+diameter_difference
@@ -39,3 +44,64 @@ class ColumnMesh(BaseMesh):
     @property
     def radius(self):
         return self.__radius
+
+
+class CubicMesh(BaseMesh):
+    def __init__(self,
+                 height: float,
+                 length: float,
+                 width: float,
+                 square_n_points: int = 20,
+                 x_center: float = 0.0,
+                 y_center: float = 0.0):
+        super().__init__(x_center=x_center,
+                         y_center=y_center,
+                         height=height,
+                         n_points=square_n_points)
+        self.__length = length
+        self.__width = width
+        self.create_cloud_points()
+
+    def create_cloud_points(self):
+        """
+        Creating the points. It consists of top and bottom sides (that's why there's a loop with 2 elements)
+        :return: cloud points is updated.
+        """
+        previous_cloud = None
+        for i in range(2):
+            local_height = i*self.height
+            new_rectangle = RectangularMesh(
+                n_points=self.n_points,
+                width=self.height,
+                length=self.length,
+                height=local_height,
+                x_center=self.x_center,
+                y_center=self.y_center)
+            cloud_points = new_rectangle.cloud_points
+            if i == 0:
+                self.cloud_points = cloud_points
+            else:
+                previous_cloud.child = new_rectangle.cloud_points
+                new_rectangle.cloud_points.parent = previous_cloud
+            previous_cloud = cloud_points
+
+    # Getters and setters
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, new_length: float):
+        self.positive_checker(new_length, "Length of cube")
+        self.__length = new_length
+        self.create_cloud_points()
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, new_width: float):
+        self.positive_checker(new_width, "Width of cube")
+        self.__width = new_width
+        self.create_cloud_points()
--- tests/integration/test_composites.py
+++ tests/integration/test_composites.py
@@ -1,4 +1,4 @@
-from structure_builder.composites import ColumnMesh
+from structure_builder.composites import ColumnMesh, CubicMesh
 import matplotlib.pyplot as plt
 
 
@@ -22,6 +22,20 @@ def test_column_displaced():
                              circle_n_points=50, x_center=2.0, y_center=-3.0)
     ax = plt.axes(projection='3d')
     current_mesh = column_mesh.cloud_points
+    while current_mesh is not None:
+        level = current_mesh.coordinates_dict
+        ax.scatter3D(list(level['x'].values()),
+                     list(level['y'].values()),
+                     list(level['z'].values()))
+        current_mesh = current_mesh.child
+    plt.show()
+    assert True
+
+
+def test_cube_printed_properly():
+    cubic_mesh = CubicMesh(height=5, length=10, width=4)
+    ax = plt.axes(projection='3d')
+    current_mesh = cubic_mesh.cloud_points
     while current_mesh is not None:
         level = current_mesh.coordinates_dict
         ax.scatter3D(list(level['x'].values()),
